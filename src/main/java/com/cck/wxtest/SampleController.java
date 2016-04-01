4f25d7ddf94964750d44a288200769ddf192bb2c
caochengkai <caochengkai@10.200.152.16>
2016-04-01 16:58:48 +0800

    ...

--- src/main/java/com/cck/wxtest/SampleController.java
+++ src/main/java/com/cck/wxtest/SampleController.java
@@ -2,17 +2,24 @@ package com.cck.wxtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class SampleController {
+public class SampleController extends SpringBootServletInitializer {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	String home() {
 		return "Hello World!";
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SampleController.class);
 	}
 
 	public static void main(String[] args) throws Exception {
--- src/main/java/com/cck/wxtest/controller/WechatController.java
+++ src/main/java/com/cck/wxtest/controller/WechatController.java
@@ -1,26 +1,34 @@
 package com.cck.wxtest.controller;
 
+import io.github.elkan1788.mpsdk4j.repo.com.qq.weixin.mp.aes.AesException;
+import io.github.elkan1788.mpsdk4j.repo.com.qq.weixin.mp.aes.SHA1;
+
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.qq.weixin.mp.aes.AesException;
-import com.qq.weixin.mp.aes.WXBizMsgCrypt;
-
 @RestController
 @RequestMapping(value = "/wechat")
 public class WechatController {
-	@Value("wechat.token")
+	@Value("${wechat.token}")
 	private String token;
-	@Value("wechat.appId")
+	@Value("${wechat.appId}")
 	private String appId;
-	@Value("wechat.encodingAesKey")
+	@Value("${wechat.encodingAesKey}")
 	private String encodingAesKey;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String valid(String signature, String timestamp, String nonce, String echostr) throws AesException {
-		WXBizMsgCrypt tool = new WXBizMsgCrypt(token, new String(new char[43]), appId);
-		return tool.verifyUrl(signature, timestamp, nonce, echostr);
+		String[] array = new String[] { token, timestamp, nonce };
+		Arrays.sort(array);
+		String result = SHA1.calculate(array[0] + array[1] + array[2]);
+		if (result.equals(signature)) {
+			return echostr;
+		} else {
+			throw new AesException(AesException.ValidateSignatureError);
+		}
 	}
 }
