5d8d28e996aa5246d5353d9c52a663c04325a75f
caochengkai <caochengkai@10.200.152.16>
2016-04-08 14:49:37 +0800

    。。

---
+++ src/test/java/com/cck/wxtest/integration/Menu.groovy
@@ -0,0 +1,19 @@
+package com.cck.wxtest.integration
+
+import org.apache.commons.io.IOUtils
+
+import com.cck.wxtest.controller.WechatController
+import com.cck.wxtest.utils.HttpClient
+import com.fasterxml.jackson.databind.ObjectMapper
+
+
+def token = 'test'
+def appId = 'wx932e17f406a7efc3'
+def appsecret = 'af9851ad6ab66ead87c1a38c393f368e'
+def encodingAesKey
+def url= 'https://api.weixin.qq.com'
+def wechatController=new WechatController(["token":token,"appId":appId,"encodingAesKey":encodingAesKey,"appsecret":appsecret,"url":url]);
+
+def accessToken=wechatController.getAccessToken();
+
+HttpClient.post(url+"/cgi-bin/menu/create?access_token="+accessToken,new ObjectMapper().readValue(IOUtils.toString(Menu.class.getResourceAsStream("menu.json"), "UTF-8"), Map.class));
