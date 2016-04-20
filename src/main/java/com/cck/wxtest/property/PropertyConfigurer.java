2f052c9ea6883295596732363406a3376d5b55b2
caochengkai <caochengkai@10.200.152.64>
2016-04-20 13:54:27 +0800

    ...

--- src/main/java/com/cck/wxtest/property/PropertyConfigurer.java
+++ src/main/java/com/cck/wxtest/property/PropertyConfigurer.java
@@ -1,15 +1,23 @@
 package com.cck.wxtest.property;
 
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import java.io.File;
+
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PropertyConfigurer extends PropertyPlaceholderConfigurer {
+public class PropertyConfigurer extends PropertySourcesPlaceholderConfigurer {
+	private static final String WXTEST_CONFIG = "wxtest.config";
+	private static final String FILE_NAME = "config.properties";
 
 	public PropertyConfigurer() {
-		Resource resource1 = new ClassPathResource("application.properties");
-		setLocations(resource1);
+		String configPath = System.getProperty(WXTEST_CONFIG);
+		if (configPath == null) {
+			throw new IllegalArgumentException("Please use -D" + WXTEST_CONFIG + "=<your config path> to set config.properties");
+		}
+		FileSystemResource configResource = new FileSystemResource(configPath + File.separator + FILE_NAME);
+		setLocations(new ClassPathResource("application.properties"), configResource);
 	}
 }
