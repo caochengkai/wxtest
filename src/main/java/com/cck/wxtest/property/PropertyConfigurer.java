d26aa13f14c067444210d7cfdbfe5f920ed119b1
caochengkai <caochengkai@10.200.152.64>
2016-04-29 17:57:49 +0800

    。。。

--- src/main/java/com/cck/wxtest/property/PropertyConfigurer.java
+++ src/main/java/com/cck/wxtest/property/PropertyConfigurer.java
@@ -3,7 +3,6 @@ package com.cck.wxtest.property;
 import java.io.File;
 
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +12,13 @@ public class PropertyConfigurer extends PropertySourcesPlaceholderConfigurer {
 	private static final String FILE_NAME = "config.properties";
 
 	public PropertyConfigurer() {
+		this.localOverride = true;
+		this.ignoreUnresolvablePlaceholders = true;
 		String configPath = System.getProperty(WXTEST_CONFIG);
 		if (configPath == null) {
 			throw new IllegalArgumentException("Please use -D" + WXTEST_CONFIG + "=<your config path> to set config.properties");
 		}
 		FileSystemResource configResource = new FileSystemResource(configPath + File.separator + FILE_NAME);
-		setLocations(new ClassPathResource("application.properties"), configResource);
+		setLocations(configResource);
 	}
 }
